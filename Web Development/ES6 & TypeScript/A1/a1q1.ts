9facf99c92d4837a048b003352ddbab6c7045763
Patel Neel Mahesh <77996149+PatelNeelMahesh@users.noreply.github.com>
2021-02-27 23:55:54 +0530

    Add files via upload

---
+++ Web Development/ES6 & TypeScript/A1/a1q1.ts
@@ -0,0 +1,10 @@
+//declaring a constant
+const num = 100;
+console.log(num);
+
+//changing the value of constant 
+num = 101;
+console.log(num);
+
+//a1q1.ts:6 Uncaught TypeError: Assignment to constant variable
+// constant's valye cannot be changed.
