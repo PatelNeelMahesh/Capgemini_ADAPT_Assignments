b5cae7c1e21e9f1c1cd7b819be32ef514b03a955
Patel Neel Mahesh <77996149+PatelNeelMahesh@users.noreply.github.com>
2021-03-15 20:59:54 +0530

    Add files via upload

---
+++ Web Development/ES6 & TypeScript/A3/a3q2.ts
@@ -0,0 +1,41 @@
+//class 
+/*
+id,name,balance
+2 sub classes
+1.SavingAccount: interest 
+2.CurrentAccount: cash_credit
+create multiple s$c account objects
+function to find total balance
+*/
+
+class Account{
+	constructor(id,name,balance){
+		this.id = id;
+		this.name = name;
+		this.balance = balance;
+	}
+}
+
+class SavingAccount extends Account{
+	constructor(id,name,balance,interest){
+		super(id,name,balance);
+		this.interest = interest;
+	}
+}
+
+class CurrentAccount extends Account{
+	constructor(id,name,balance,cash_credit){
+		super(id,name,balance);
+		this.cash_credit = cash_credit;
+	}
+}
+
+let sobj = new SavingAccount(1,"one",1000,10); 
+let cobj = new CurrentAccount(1,"one",1000,500); 
+
+function total_balance(){
+	let total = sobj.balance-cobj.cash_credit;
+	return total;
+}
+
+console.log(total_balance());
