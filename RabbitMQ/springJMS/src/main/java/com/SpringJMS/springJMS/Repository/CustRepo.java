e01ffb8da444bae73d4c924d979506f0028e7899
Patel Neel Mahesh <77996149+PatelNeelMahesh@users.noreply.github.com>
2021-03-31 03:58:02 +0530

    Delete CustRepo.java

--- RabbitMQ/springJMS/src/main/java/com/SpringJMS/springJMS/Repository/CustRepo.java
+++
@@ -1,9 +0,0 @@
-package com.SpringJMS.springJMS.Repository;
-
-import com.SpringJMS.springJMS.Model.Customer;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustRepo extends MongoRepository<Customer,String> {
-}
